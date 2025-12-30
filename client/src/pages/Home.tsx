a2791d49f0c66aa42a7e11dc7dda8a5ee151fcf3
“Muhib <muhibwaqar@hotmail.com>
2025-12-30 11:25:02 +0500

    Add fact about starting coding in grade 7

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -13,6 +13,7 @@ export default function Home() {
   // Random facts about Afifa
   const afifaFacts = [
     "Afifa is studying Computer Engineering at the University of Waterloo",
+    "Afifa started her coding journey in grade 7 - a true early starter!",
     "Afifa has expertise in React, Node.js, and TypeScript",
     "Afifa builds innovative full-stack applications with modern web technologies",
     "Afifa is passionate about AI integration and creative problem-solving",
